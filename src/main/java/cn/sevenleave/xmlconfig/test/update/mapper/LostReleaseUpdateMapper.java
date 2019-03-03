13e7c36268e9ebf56ca816ff8c355f67bfeaa0b3
zouming <zouming@souche.com>
2019-03-03 11:18:44 +0800

    feature:chore，测试mybatis-generator

---
+++ src/main/java/cn/sevenleave/xmlconfig/test/update/mapper/LostReleaseUpdateMapper.java
@@ -0,0 +1,7 @@
+package cn.sevenleave.xmlconfig.test.update.mapper;
+
+import cn.sevenleave.xmlconfig.test.update.model.LostReleaseUpdate;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface LostReleaseUpdateMapper extends Mapper<LostReleaseUpdate> {
+}
