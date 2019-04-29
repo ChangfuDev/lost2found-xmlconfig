6bce382764f0ef535d9173050fc6f85f8e87cc42
zouming <zouming@souche.com>
2019-04-29 09:10:51 +0800

    chore:删除test手动测试的文件

--- src/main/java/cn/sevenleave/xmlconfig/test/update/mapper/LostReleaseUpdateMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.sevenleave.xmlconfig.test.update.mapper;
-
-import cn.sevenleave.xmlconfig.test.update.model.LostReleaseUpdate;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface LostReleaseUpdateMapper extends Mapper<LostReleaseUpdate> {
-}
